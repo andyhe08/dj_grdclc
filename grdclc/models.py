0dd1f6f9a537183c1992d399ddb3de209fe01875
Andy He <andy.he@duke.edu>
2021-06-29 20:19:14 -0400

    created model for storing user data

--- grdclc/models.py
+++ grdclc/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Data(models.Model):
+    CATEGORY_CHOICES = [('Y', 'Yes'), ('N', 'No')]
+
+    username = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    isCategory = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
+    catNum = models.IntegerField()
+    assignNum = models.IntegerField(null=True)
+    name = models.CharField(max_length=100, null=True)
+
+    # Following are doubles in the database
+    weight = models.CharField(max_length=5, null=True)
+    yourScore = models.CharField(max_length=5, null=True)
+    totalScore = models.CharField(max_length=5, null=True)
